305a40837c08b4ff6a353e81bfa16bf9338d44f4
cubeninggen <hhybg@naver.com>
2023-06-10 01:53:30 +0900

    first_commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+from pymongo import MongoClient
+client = MongoClient('mongodb+srv://sparta:test@cluster0.2waamsb.mongodb.net/?retryWrites=true&w=majority')
+db = client.dbsparta
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route("/bucket", methods=["POST"])
+def bucket_post():
+    bucket_receive = request.form['bucket_give']
+
+    bucket_list = list(db.bucket.find({}, {'_id': False}))
+    count = len(bucket_list) + 1
+    doc = {
+        'num': count,
+        'bucket': bucket_receive,
+        'done': 0
+    }
+    db.bucket.insert_one(doc)
+    return jsonify({'msg': '저장완료!'})
+    
+@app.route("/bucket", methods=["GET"])
+def bucket_get():
+    all_buckets = list(db.bucket.find({},{'_id':False}))
+    return jsonify({'result': all_buckets})
+
+@app.route("/bucket/complete", methods=["POST"])
+def bucket_complete():
+    bucket_num = int(request.form['bucket_num'])
+
+    db.bucket.update_one({'num': bucket_num}, {'$set': {'done': 1}})
+
+    return jsonify({'msg': '매세지가 확인되었습니다!'})
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
