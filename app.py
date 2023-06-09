e4888e14607dadb7fa1c81b81d5c6a279f16ebce
cubeninggen <hhybg@naver.com>
2023-06-10 01:55:22 +0900

    back_up

--- app.py
+++ app.py
@@ -37,4 +37,4 @@ def bucket_complete():
     return jsonify({'msg': '매세지가 확인되었습니다!'})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5001, debug=True)
+    app.run('0.0.0.0', port=5000, debug=True)
